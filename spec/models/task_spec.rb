62cf3a289d15e84bfac7ca68fbf800af9e26a768
inasakunovel <inasaku.novel@gmail.com>
2019-11-05 13:05:44 +0900

    Fix: rspec構成変更

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,51 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  describe 'パリデーションテスト' do
+  describe 'パリデーション確認' do
     let(:user){ create :user }
 
-    describe 'タイトルのバリデーションテスト' do
-      context 'title presence:trueチェック' do
-
-        it 'タイトルが空の時にエラーになる' do
-          task = build(:task, title: '', status: 'todo', user_id: user.id)
-          expect(task).to_not be_valid
-        end
-
-        it 'タイトルが入っている時に登録できる' do
-          task = build(:task, title: 'タイトル', status: 'todo', user_id: user.id)
-          expect(task).to be_valid
-        end
-      end
-
-      context 'title uniqueness:trueチェック' do
-        before do
-          create(:task, title: 'タイトル', status: 'todo', user_id: user.id)
-        end
-
-        it 'タイトルが重複している時エラーになる' do
-          task = build(:task, title: 'タイトル', status: 'todo', user_id: user.id)
-          expect(task).to_not be_valid
-        end
-
-        it 'タイトルが重複していないと登録できる' do
-          task = build(:task, title: 'タイトル1', status: 'todo', user_id: user.id)
-          expect(task).to be_valid
-        end
+    context 'タイトルが空の場合' do
+      it 'バリデーションエラーになる' do
+        task = build(:task, title: '', status: 'todo', user_id: user.id)
+        expect(task).to_not be_valid
       end
     end
 
-    describe 'ステータスのバリデーションテスト' do
-      context 'status presence:trueチェック' do
-        it 'ステータスが空の時にエラーになる' do
-          task = build(:task,  title: 'タイトル', status: '', user_id: user.id)
-          expect(task).to_not be_valid
-        end
+    context 'タイトルが正常値の場合' do
+      it 'バリデーションエラーが発生しない' do
+        task = build(:task, title: 'タイトル', status: 'todo', user_id: user.id)
+        expect(task).to be_valid
+      end
+    end
 
-        it 'ステータスが入っている時に登録できる' do
-          task = build(:task, title: 'タイトル', status: 'todo', user_id: user.id)
-          expect(task).to be_valid
-        end
+    context 'タイトルが重複している場合' do
+      before do
+        create(:task, title: 'タイトル', status: 'todo', user_id: user.id)
+      end
+      it 'バリデーションエラーになる' do
+        task = build(:task, title: 'タイトル', status: 'todo', user_id: user.id)
+        expect(task).to_not be_valid
+      end
+    end
+
+    context 'タイトルが重複していない場合' do
+      it 'バリデーションエラーが発生しない' do
+        task = build(:task, title: 'タイトル1', status: 'todo', user_id: user.id)
+        expect(task).to be_valid
+      end
+    end
+
+    context 'ステータスが空の場合' do
+      it 'バリデーションエラーになる' do
+        task = build(:task,  title: 'タイトル', status: '', user_id: user.id)
+        expect(task).to_not be_valid
+      end
+    end
+
+    context 'ステータスが正常値の場合' do
+      it 'バリデーションエラーが発生しない' do
+        task = build(:task, title: 'タイトル', status: 'todo', user_id: user.id)
+        expect(task).to be_valid
       end
     end
   end
