2ad945c4575151ed64bd396f0cdb036a260c1b6e
inasakunovel <inasaku.novel@gmail.com>
2019-11-05 14:17:34 +0900

    Fix: associationの使用

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   describe 'パリデーション確認' do
-    let(:user){ create :user }
 
     context 'タイトルが空の場合' do
       it 'バリデーションエラーになる' do
-        task = build(:task, title: '', status: 'todo', user_id: user.id)
+        task = FactoryBot.build(:task, title: '', status: 'todo')
         expect(task).to_not be_valid
         task.valid?
         expect(task.errors.messages[:title]).to include("can't be blank")
@@ -15,17 +14,17 @@ RSpec.describe Task, type: :model do
 
     context 'タイトルが正常値の場合' do
       it 'バリデーションエラーが発生しない' do
-        task = build(:task, title: 'タイトル', status: 'todo', user_id: user.id)
+        task = FactoryBot.build(:task, title: 'タイトル', status: 'todo')
         expect(task).to be_valid
       end
     end
 
     context 'タイトルが重複している場合' do
       before do
-        create(:task, title: 'タイトル', status: 'todo', user_id: user.id)
+        FactoryBot.create(:task, title: 'タイトル', status: 'todo')
       end
       it 'バリデーションエラーになる' do
-        task = build(:task, title: 'タイトル', status: 'todo', user_id: user.id)
+        task = FactoryBot.build(:task, title: 'タイトル', status: 'todo')
         expect(task).to_not be_valid
         task.valid?
         expect(task.errors.messages[:title]).to include('has already been taken')
@@ -34,14 +33,14 @@ RSpec.describe Task, type: :model do
 
     context 'タイトルが重複していない場合' do
       it 'バリデーションエラーが発生しない' do
-        task = build(:task, title: 'タイトル1', status: 'todo', user_id: user.id)
+        task = FactoryBot.build(:task, title: 'タイトル1', status: 'todo')
         expect(task).to be_valid
       end
     end
 
     context 'ステータスが空の場合' do
       it 'バリデーションエラーになる' do
-        task = build(:task,  title: 'タイトル', status: '', user_id: user.id)
+        task = FactoryBot.build(:task,  title: 'タイトル', status: '')
         expect(task).to_not be_valid
         task.valid?
         expect(task.errors.messages[:status]).to include("can't be blank")
@@ -50,7 +49,7 @@ RSpec.describe Task, type: :model do
 
     context 'ステータスが正常値の場合' do
       it 'バリデーションエラーが発生しない' do
-        task = build(:task, title: 'タイトル', status: 'todo', user_id: user.id)
+        task = FactoryBot.build(:task, title: 'タイトル', status: 'todo')
         expect(task).to be_valid
       end
     end
